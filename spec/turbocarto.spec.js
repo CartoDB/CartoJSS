3a3e9e6d62d2f93f62391ce34da49060a9358b41
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2017-10-20 14:32:46 +0200

    Add turbocarto test

---
+++ spec/turbocarto.spec.js
@@ -0,0 +1,15 @@
+var cartojss = require('../lib/cartojss.js');
+
+describe('turbocarto', function () {
+
+  it('should serialize correctly a turbocarto ramp', function () {
+    var actual = cartojss.serialize({
+      '#selector': {
+        'marker-width': 'ramp([price], (10, 20, 30), jenks())'
+      },
+    });
+    var expected = '#selector {\n  marker-width: ramp([price], (10, 20, 30), jenks());\n}';
+    expect(actual).toEqual(expected);
+  });
+
+});
